0bbe22795a2d898c3885db359e89bae6f4a57ea5
Romario Negreiros <nromario482@gmail.com>
2023-01-22 21:08:19 -0300

    Add register method which creates a new user in database and sends an email in order to verify its email address

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,14 +1,13 @@
 import { User } from '../models'
 import crypto from 'crypto'
 
-import checkNullFields from '../utils/checkNullFields'
+import { mailer, checkNullFields } from '../utils'
 
 import type { Request, Response } from 'express'
 
 class AuthController {
   public async register(req: Request, res: Response) {
     const nullFields = checkNullFields(req.body)
-    console.log(req.body)
     if (nullFields?.length) {
       return res.status(400).json({
         error: {
@@ -23,19 +22,28 @@ class AuthController {
         return res.status(400).json({ error: 'This email is already in use!' })
       }
 
-      // const verifyEmailToken = crypto.randomBytes(20).toString('hex')
-      // const verifyEmailTokenExpiration = new Date()
+      const verifyEmailToken = crypto.randomBytes(20).toString('hex')
+      const verifyEmailTokenExpiration = new Date()
 
       req.body.email.toLowerCase()
       const user = await User.create({
-        ...req.body
+        ...req.body,
+        verifyEmailToken,
+        verifyEmailTokenExpiration
+      })
+
+      await mailer.send([user.email], 'Account Creation', 'verifyEmail', {
+        link: `http://localhost:3434/?token=${verifyEmailToken}&email=${user.email}`,
+        name: user.name
       })
 
       return res.status(201).json({ ...user.dataValues, pwd: undefined })
     } catch (err) {
+      if (await User.findOne({ where: { email: req.body.email } })) {
+        await User.destroy({ where: { email: req.body.email } })
+      }
       return res.status(500).json({ error: err.message })
     }
-
   }
 }
 
