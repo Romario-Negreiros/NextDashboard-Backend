698c2cbb4b8704e2d3f0f2d604b246df7b490eb4
Romario Negreiros <nromario482@gmail.com>
2023-01-25 21:46:27 -0300

    Rename file to router

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-import { authController } from './controllers'
-
-const routes = express.Router()
-
-routes.post('/register', authController.register)
-
-export default routes
